b54475d98bbefc6a539e57b2ab826ab8d33afc0b
AlbertoJTD <albertojtd@outlook.com>
2022-12-31 23:35:05 -0600

    Edit & update a message without refreshing the page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "messages#index"
-  resources :messages
+  resources :messages do
+    member do
+      post :edit
+    end
+  end
 
 end
